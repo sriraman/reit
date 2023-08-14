ac47475958daf914483d580b4ba6614647e62a46
Sriraman Panneerselvam <sriramancse@gmail.com>
2023-08-14 11:26:27 +0530

    broke down the tasks to speed up the load time

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { NextUIProvider, Table, Container, Card, Text, Badge, Link, Image, Loadi
 import ReactGA from 'react-ga4';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import axios from 'axios';
 
 const firebaseConfig = {
   apiKey: "AIzaSyB-1puLjbQCszlVoaJA8IqPNXjMVNg9y1w",
@@ -56,27 +57,29 @@ export default function Home() {
   React.useEffect(() => {
     setWidth(window.innerWidth);
 
-    fetch('https://api.dalalstreet.pro/getQuote?api_key=CssgK3JQNenGzU6aDTr6w6g5S&symbols='+scrips.join(","))
-      .then((response => {
-        if (response.ok) {
-          return response.json();
+    const requests = scrips.map(async (symbol) => {
+      if (symbol) {
+        const url = "https://api.dalalstreet.pro/getQuote?api_key=CssgK3JQNenGzU6aDTr6w6g5S&symbols=" + symbol;
+        return axios.get(url);
+      }
+    })
+
+    Promise.all(requests).then((resp) => {
+
+      let price = [];
+
+      resp.map((response) => {
+        let data = response.data[0];
+        price[data.scrip] = {
+          scrip: data.scrip,
+          ltp: data.price,
+          name: data.name
         }
-        throw response;
-      }))
-      .then(data => {
-        let price = [];
+      });
 
-        data.map((d) => {
-          price[d.scrip] = {
-            scrip: d.scrip,
-            ltp: d.price,
-            name: d.name
-          }
-        });
+      setPriceData(price);
 
-        setPriceData(price);
-      })
-
+    });
 
     ReactGA.initialize('G-Q5V6YP2Y0C');
 
